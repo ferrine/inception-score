684d35a7d1fef0e771cb0f11c2d387516fc30df6
Yuchen Li <ychnlgy@gmail.com>
2018-09-12 16:39:29 -0400

    Uploaded base CIFAR10 computation

--- image_feeder.py
+++ image_feeder.py
@@ -1,20 +1,32 @@
-import numpy, os
+import numpy, os, tqdm
 
+import scipy.misc
 import torchvision.datasets
 
 import inception_score
 
 ROOT = "data"
 
-def get_cifar10(download):
-    download = int(download)
-    train = torchvision.datasets.CIFAR10(root=ROOT, train=True, download=download)
-    data = numpy.transpose(train.train_data, axes=[0, 3, 1, 2]).astype(numpy.float32)
-    data = (data/255.0-0.5)*2 # scale between -1 and 1
-    return data, train.train_labels
+def get_cifar10(folder=None, download=False):
+    if folder is None:
+        download = int(download)
+        train = torchvision.datasets.CIFAR10(root=ROOT, train=True, download=download)
+        data = numpy.transpose(train.train_data, axes=[0, 3, 1, 2]).astype(numpy.float32)
+        
+    else:
+        images = []
+        for f in tqdm.tqdm(os.listdir(folder), desc="Loading images", ncols=80):
+            fname = os.path.join(folder, f)
+            im = scipy.misc.imread(fname)
+            images.append(im)
+        data = numpy.array(images)
+        
+    data = data.astype(numpy.float32)
+    data = (data/255.0-0.5)*2
+    return data
 
-def compute_inception(download):
-    data, labs = get_cifar10(download)
+def compute_inception(folder, download):
+    data, labs = get_cifar10(folder, download)
     output = inception_score.get_inception_score(data)
     print("Mean [std dev]: %s [%s]" % output)
 
@@ -22,7 +34,8 @@ def main():
     import sys
     
     DEFAULT = {
-        "download": 0
+        "download": 0,
+        "folder": None
     }
     
     args = [a.split("=") for a in sys.argv[1:]]
